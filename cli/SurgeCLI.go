b27e5c6a6108215210ab900ec13f2ace58fbfc06
Andy Rea <email@andrewrea.co.uk>
2019-11-18 08:47:09 +0000

    Adding some output

--- cli/SurgeCLI.go
+++ cli/SurgeCLI.go
@@ -50,8 +50,28 @@ func Min(values []int64) (result int64) {
 	return
 }
 
+func ReadFileToLines(path string) (lines []string, err error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return
+	}
+	lines = []string{}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+	err = scanner.Err()
+	return
+}
+
+type SurgeServiceClientConnection struct {
+	Client     server.SurgeServiceClient
+	Connection *grpc.ClientConn
+}
+
 type SurgeCLI struct {
-	workers []server.SurgeServiceClient
+	workers []SurgeServiceClientConnection
 	//TODO: Create a configuration struct for these
 	urlFilePath string
 	server      bool
@@ -63,79 +83,99 @@ type SurgeCLI struct {
 	iterations  int
 }
 
+func (surgeCLI *SurgeCLI) StartServer(port int) SurgeServiceClientConnection {
+	cmd := exec.Command("./surge", "--server", "--server-host", "localhost", "--server-port", strconv.Itoa(port))
+	cmd.Stdout = os.Stdout
+	cmd.Start()
+	//This would be better to have a synchronous wait timer
+	// that would panic after a given threshold.
+	// e.g. WaitFor(endpoint, 10 * time.Second)
+	// and maybe tie this into the PingResponse to assert
+	// on Healthy
+	time.Sleep(1 * time.Second)
+
+	conn, err := grpc.Dial(fmt.Sprintf("localhost:%d", port), grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+
+	client := server.NewSurgeServiceClient(conn)
+
+	return SurgeServiceClientConnection{
+		Connection: conn,
+		Client:     client,
+	}
+}
+
+func (surgeCLI *SurgeCLI) RunServer() (result *service.SurgeResult, err error) {
+	server.StartServer(fmt.Sprintf("%v:%v", surgeCLI.serverHost, surgeCLI.serverPort))
+	return
+}
+
+func (surgeCLI *SurgeCLI) RunController() (result *service.SurgeResult, err error) {
+	var lines []string
+
+	if surgeCLI.urlFilePath == "" {
+		panic("No URL file supplied")
+	}
+
+	lines, err = ReadFileToLines(surgeCLI.urlFilePath)
+	if err != nil {
+		panic(err)
+	}
+	var wg = sync.WaitGroup{}
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	responses := make(chan *server.SurgeResponse, surgeCLI.processes)
+
+	//Step 1:  Start all the services and store in the array
+
+	fmt.Println("Starting the worker processes...")
+
+	for i := 0; i < surgeCLI.processes; i++ {
+		portNumber := 54322 + i
+		connection := surgeCLI.StartServer(portNumber)
+		defer func() {
+			connection.Client.Kill(ctx, &empty.Empty{})
+			connection.Connection.Close()
+		}()
+		surgeCLI.workers = append(surgeCLI.workers, connection)
+	}
+
+	fmt.Println("Surging...")
+
+	for _, connection := range surgeCLI.workers {
+		wg.Add(1)
+		go func(client server.SurgeServiceClient) {
+			response, err := client.Run(ctx, &server.SurgeRequest{
+				Iterations:  int32(surgeCLI.iterations),
+				Lines:       lines,
+				Random:      surgeCLI.random,
+				WorkerCount: int32(surgeCLI.workerCount),
+			})
+			responses <- response
+
+			if err != nil {
+				panic(err)
+			}
+			wg.Done()
+		}(connection.Client)
+	}
+
+	wg.Wait()
+
+	result = surgeCLI.MergeResponses(responses)
+	return
+}
+
 func (surgeCLI *SurgeCLI) Run() (result *service.SurgeResult, err error) {
 	if surgeCLI.server {
-		//Start the server
-		server.StartServer(fmt.Sprintf("%v:%v", surgeCLI.serverHost, surgeCLI.serverPort))
-	} else {
-		var file *os.File
-		var lines []string
-		// Parsing the file is a concern of the CLI not the service.
-		if surgeCLI.urlFilePath != "" {
-			file, err = os.Open(surgeCLI.urlFilePath)
-			if err != nil {
-				return
-			}
-			lines = []string{}
-			scanner := bufio.NewScanner(file)
-			for scanner.Scan() {
-				line := scanner.Text()
-				lines = append(lines, line)
-			}
-			if err = scanner.Err(); err != nil {
-				return
-			}
-			var wg = sync.WaitGroup{}
-			ctx, cancel := context.WithCancel(context.Background())
-			defer cancel()
-
-			responses := make(chan *server.SurgeResponse, surgeCLI.processes)
-			for i := 0; i < surgeCLI.processes; i++ {
-				wg.Add(1)
-				portNumber := 54322 + i
-				go func(port int) {
-					cmd := exec.Command("./surge", "--server", "--server-host", "localhost", "--server-port", strconv.Itoa(port))
-					cmd.Stdout = os.Stdout
-					cmd.Start()
-					//This would be better to have a synchronous wait timer
-					// that would panic after a given threshold.
-					// e.g. WaitFor(endpoint, 10 * time.Second)
-					// and maybe tie this into the PingResponse to assert
-					// on Healthy
-					time.Sleep(1 * time.Second)
-
-					conn, err := grpc.Dial(fmt.Sprintf("localhost:%d", port), grpc.WithInsecure())
-					if err != nil {
-						log.Fatalf("did not connect: %v", err)
-
-					}
-					defer conn.Close()
-
-					client := server.NewSurgeServiceClient(conn)
-					defer client.Kill(ctx, &empty.Empty{})
-
-					response, err := client.Run(ctx, &server.SurgeRequest{
-						Iterations:  int32(surgeCLI.iterations),
-						Lines:       lines,
-						Random:      surgeCLI.random,
-						WorkerCount: int32(surgeCLI.workerCount),
-					})
-					responses <- response
-
-					if err != nil {
-						panic(err)
-					}
-					wg.Done()
-				}(portNumber)
-				//Try to connect to the server and once connected add the connection to the array and start the next worker
-			}
-
-			wg.Wait()
-
-			result = surgeCLI.MergeResponses(responses)
-		}
+		return surgeCLI.RunServer()
 	}
-	return
+
+	return surgeCLI.RunController()
 }
 
 func (surgeCLI SurgeCLI) MergeResponses(responses chan *server.SurgeResponse) (result *service.SurgeResult) {
